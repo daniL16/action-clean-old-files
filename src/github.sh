2507af7bb847cb0f2cfb62bad3eff6df45102feb
dani <dani16lp@gmail.com>
2021-02-24 18:20:35 +0100

    init commit

---
+++ src/github.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+GITHUB_API_URI="https://api.github.com"
+GITHUB_API_HEADER="Accept: application/vnd.github.v3+json"
+
+github::create_pr(){
+
+}
---
+++ src/main.sh
@@ -0,0 +1,12 @@
+source "$PR_SIZE_LABELER_HOME/src/github.sh"
+
+main(){
+
+export GITHUB_TOKEN="$1"
+export DIR="$3"
+export DAYS="$5"
+
+# borrar los archivos con más de x dias de antiguedad
+find "${DIR}" -type f -mtime +"${DAYS}" -exec rm {} \;
+
+}
