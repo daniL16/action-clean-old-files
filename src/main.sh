6fa996fcdfa44b02f6408799a8344d8088be4ccd
Dani Lopez Garcia <dani16lp@gmail.com>
2021-03-01 13:01:23 +0100

    Update main.sh

--- src/main.sh
+++ src/main.sh
@@ -5,6 +5,7 @@ main(){
 export GITHUB_TOKEN="$1"
 export DIR="$2"
 export DAYS="$4"
+export PR_TITLE="$3"
 
 git checkout -b delete_old_migrations;
 
@@ -22,11 +23,12 @@ do
   fi
 done;
 
+git fetch --all;
 git add . ;
 git -c user.name="GitHub Actions" -c user.email="actions@github.com" \
         commit -m "Delete old migrations" ;
 git push --set-upstream origin "HEAD:delete_old_migrations";
 
-github::create_pr $3 'delete_old_migrations'
+github::create_pr ${PR_TITLE} 'delete_old_migrations'
 
 }
